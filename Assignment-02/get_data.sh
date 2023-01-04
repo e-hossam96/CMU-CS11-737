5f6052e710a90fa0fad9f2455903f20b03349812
e-hossam96 <ehh.hossam@gmail.com>
2023-01-04 20:55:31 +0000

    Updated get_data.sh

--- Assignment-02/get_data.sh
+++ Assignment-02/get_data.sh
@@ -1,3 +1,5 @@
+wget https://raw.githubusercontent.com/neulab/word-embeddings-for-nmt/master/ted_reader.py
+
 mkdir -p data
 cd data
 
@@ -5,5 +7,3 @@ wget http://phontron.com/data/ted_talks.tar.gz
 tar -xvf ted_talks.tar.gz
 
 cd ..
-
-python ted_reader.py
